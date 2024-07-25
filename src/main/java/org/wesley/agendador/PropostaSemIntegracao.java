d3835faf22add249a0e50f06c1f53f0a78925f3c
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-25 11:01:41 -0300

    Finalizando implementação do Scheduled para processamento das propostas não integradas

--- src/main/java/org/wesley/agendador/PropostaSemIntegracao.java
+++ src/main/java/org/wesley/agendador/PropostaSemIntegracao.java
@@ -1,13 +1,21 @@
 package org.wesley.agendador;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.wesley.entity.Proposta;
 import org.wesley.repository.PropostaRepository;
 import org.wesley.service.NotificacaoRabbitService;
 
+import java.util.concurrent.TimeUnit;
+
 @Component
 public class PropostaSemIntegracao {
+
+    private final Logger logger = LoggerFactory.getLogger(PropostaSemIntegracao.class);
 
     @Autowired
     private PropostaRepository propostaRepository;
@@ -18,16 +26,20 @@ public class PropostaSemIntegracao {
     @Value("${rabbitmq.propostapendente.exchange}")
     private String exchange;
 
+    @Scheduled(fixedDelay = 10, timeUnit = TimeUnit.SECONDS)
     public void buscarPropostaSemIntegracao() {
         propostaRepository.findAllByIntegradaIsFalse().forEach( proposta -> {
             try {
                 notificacaoRabbitService.notificar(proposta, exchange);
-                proposta.setIntegrada(true);
-                propostaRepository.save(proposta);
+                atualizarProposta(proposta);
             } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
+                logger.error(e.getMessage());
             }
-
         });
+    }
+
+    private void atualizarProposta(Proposta proposta) {
+        proposta.setIntegrada(true);
+        propostaRepository.save(proposta);
     }
 }
