4be4c28cba7af3d5b8102a5a64ae06805097c7e9
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-08-01 10:15:06 -0300

    Criando microserviço de análise de credito

---
+++ AnaliseCreditoMS/src/main/java/org/wesley/AnaliseCreditoMsApplication.java
@@ -0,0 +1,11 @@
+package org.wesley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnaliseCreditoMsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnaliseCreditoMsApplication.class, args);
+    }
+}
